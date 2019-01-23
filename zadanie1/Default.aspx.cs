32768f875d29faa9076dee623311bb6f6d80ef29
Your Name <prokurat.p@gmail.com>
2019-01-23 18:58:23 +0100

    mnożenie macierzy

--- zadanie1/Default.aspx.cs
+++ zadanie1/Default.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace zadanie1
 {
@@ -49,6 +51,91 @@ namespace zadanie1
             return macierz;
         }
 
+        private void MnozenieMacierzy(double[,] m1, double[,] m2)
+        {           
+            int m1_wiersze = m1.GetLength(0);
+            int m1_kolumny = m1.GetLength(1);
+            int m2_wiersze = m2.GetLength(0);
+            int m2_kolumny = m2.GetLength(1);
+
+            double[,] result;
+
+            if ((m1_kolumny != m2_wiersze) && (m2_kolumny != m1_wiersze))
+            {
+                Response.Write("Nieodpowiednie wymiary macierzy! Ilość kolumn pierwszej macierzy musi odpowiadać ilości wierszy drugiej macierzy.");
+            }
+            else if ((m1_kolumny != m2_wiersze) && (m2_kolumny == m1_wiersze))
+            {
+                double[,] m1_new = new double[m2_wiersze, m2_kolumny];
+                double[,] m2_new = new double[m1_wiersze, m1_kolumny];
+
+                for (int i = 0; i < m1_wiersze; i++)
+                {
+                    for (int j = 0; j < m1_kolumny; j++)
+                    {
+                        m2_new[i, j] = m1[i, j];
+                    }
+                }
+
+                for (int i = 0; i < m2_wiersze; i++)
+                {
+                    for (int j = 0; j < m2_kolumny; j++)
+                    {
+                        m1_new[i, j] = m2[i, j];
+                    }
+                }
+
+                result = new double[m2_wiersze, m1_kolumny];
+
+                for (int i = 0; i < m2_wiersze; i++)
+                {
+                    for (int j = 0; j < m1_kolumny; j++)
+                    {
+                        for (int k = 0; k < m2_kolumny; k++)
+                        {
+                            result[i, j] += m1_new[i, k] * m2_new[k, j];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < m2_wiersze; i++)
+                {
+                    for (int j = 0; j < m1_kolumny; j++)
+                    {
+                        Response.Write(result[i, j] + "/");
+                    }
+                    Response.Write("//");
+                }
+            }
+            else
+            {
+                result = new double[m1_wiersze, m2_kolumny];
+
+                for (int i = 0; i < m1_wiersze; i++)
+                {
+                    for (int j = 0; j < m2_kolumny; j++)
+                    {
+                        for (int k = 0; k < m1_kolumny; k++)
+                        {
+                            result[i, j] += m1[i, k] * m2[k, j];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < m1_wiersze; i++)
+                {
+                    for (int j = 0; j < m2_kolumny; j++)
+                    {
+                        Response.Write(result[i, j] + "/");
+                    }
+                    Response.Write("//");
+                }
+            }
+
+            
+
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -59,6 +146,7 @@ namespace zadanie1
             double[,] macierz1 = WczytajMacierz("UploadedFiles/macierz1.txt");
             double[,] macierz2 = WczytajMacierz("UploadedFiles/macierz2.txt");
 
+            MnozenieMacierzy(macierz1,macierz2);
         }
 
 
